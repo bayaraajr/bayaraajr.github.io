29a46b53e74c7c49d53471769b9cb09244cea84f
Bayarjargal.J <bayarjargal.jr@gmail.com>
2022-09-01 05:49:34 +0000

    Updates

---
+++ hi-five.js
@@ -0,0 +1,28 @@
+"use strict";
+(self["webpackChunkbayaraajr_github_io"] = self["webpackChunkbayaraajr_github_io"] || []).push([["hi-five"],{
+
+/***/ "./src/pages/hi-five/index.tsx":
+/*!*************************************!*\
+  !*** ./src/pages/hi-five/index.tsx ***!
+  \*************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+
+const HiFive = ({ ...props }) => {
+    return react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", null,
+        react__WEBPACK_IMPORTED_MODULE_0___default().createElement("p", null, "Don't leave me hangin "),
+        react__WEBPACK_IMPORTED_MODULE_0___default().createElement("button", null, "\uD83D\uDC4B"));
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (HiFive);
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=hi-five.js.map
---
+++ home.js
@@ -0,0 +1,31 @@
+"use strict";
+(self["webpackChunkbayaraajr_github_io"] = self["webpackChunkbayaraajr_github_io"] || []).push([["home"],{
+
+/***/ "./src/pages/home/index.tsx":
+/*!**********************************!*\
+  !*** ./src/pages/home/index.tsx ***!
+  \**********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router/index.js");
+
+
+const Home = ({ ...props }) => {
+    const navigate = (0,react_router_dom__WEBPACK_IMPORTED_MODULE_1__.useNavigate)();
+    return react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", null,
+        react__WEBPACK_IMPORTED_MODULE_0___default().createElement("p", null, "Hi there \uD83D\uDC4B"),
+        react__WEBPACK_IMPORTED_MODULE_0___default().createElement("button", { onClick: () => navigate('/hifive') }, "Gimme high five"));
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Home);
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=home.js.map
