df933c8f73bbcb9f575ed876b02c2a5f6607d9ae
Bayarjargal.J <bayarjargal.jr@gmail.com>
2022-11-14 03:46:40 +0000

    Updates

--- home.js
+++ home.js
@@ -13,14 +13,11 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router/index.js");
-
 
 const Home = ({ ...props }) => {
-    const navigate = (0,react_router_dom__WEBPACK_IMPORTED_MODULE_1__.useNavigate)();
     return react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", null,
         react__WEBPACK_IMPORTED_MODULE_0___default().createElement("p", null, "Hi there \uD83D\uDC4B"),
-        react__WEBPACK_IMPORTED_MODULE_0___default().createElement("button", { onClick: () => navigate('/hifive') }, "Gimme high five"));
+        react__WEBPACK_IMPORTED_MODULE_0___default().createElement("p", null, "This page is under construction."));
 };
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Home);
 
